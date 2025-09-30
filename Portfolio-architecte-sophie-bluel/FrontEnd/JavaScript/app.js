dc7957f24b625f437d9ddd0d2344ff241ed5c16a
lucie-devcode <lucie.devcode@gmail.com>
2025-09-30 23:00:01 +0200

    debut setFilter

--- Portfolio-architecte-sophie-bluel/FrontEnd/JavaScript/app.js
+++ Portfolio-architecte-sophie-bluel/FrontEnd/JavaScript/app.js
@@ -1,14 +1,13 @@
 async function loadProjets() {
    const url = "http://localhost:5678/api/works";
 
-   try {
+   try { //gérer les erreurs
       const response = await fetch(url);
       if (!response.ok) {
          throw new Error(`Erreur ${response.status} : impossible de récupérer les projets`);
       }
 
       const projets = await response.json();
-      console.log(projets);
 
       for (let i = 0; i < projets.length; i++) {
          setFigure(projets[i]);
@@ -27,3 +26,42 @@ function setFigure(projet) {
    `;
    document.querySelector(".gallery").append(figure);
 }
+
+async function loadCategories() {
+   const url = "http://localhost:5678/api/categories";
+
+   try { //gérer les erreurs
+      const response = await fetch(url);
+      if (!response.ok) {
+         throw new Error(`Erreur ${response.status} : impossible de récupérer les projets`);
+      }
+
+      const projets = await response.json();
+      console.log(projets);
+
+      for (let i = 0; i < projets.length; i++) {
+         setFilter(projets[i]);
+      }
+   } catch (error) {
+      console.error(error.message);
+   }
+}
+loadCategories();
+
+
+const tous = document.createElement("div");
+tous.innerHTML = "Tous";
+tous.className = "btn-tous";
+tous.addEventListener("click", () => {
+    loadProjets("all");
+});
+document.querySelector(".categories").append(tous);
+
+function setFilter(data) {
+   const div = document.createElement("div");
+   div.innerHTML = `${data.name}`;
+   document.querySelector(".categories").append(div);
+}
+
+
+
