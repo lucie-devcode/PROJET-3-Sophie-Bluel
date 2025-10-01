2b30d143668561a107209d37a37fb1e0460bd6ae
lucie-devcode <lucie.devcode@gmail.com>
2025-10-01 14:52:36 +0200

    login css fini

---
+++ Portfolio-architecte-sophie-bluel/FrontEnd/JavaScript/login.js
@@ -0,0 +1,8 @@
+const loginApi = "http://localhost:5678/api/users/login";
+const submitButton = document.querySelector("#submitButton");
+
+document.querySelector("#submitButton").addEventListener("click", () => handleSubmit());
+
+function handleSubmit(email, password) {
+    emai
+}
